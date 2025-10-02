f0901951b8179ca9fe75cbf0a6670f5ec46651e5
COMRADE040 <rafiulislam040@gmail.com>
2025-10-02 21:46:25 +0600

    show details

---
+++ src/Components/PostDetail/Postdetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useLoaderData } from 'react-router';
+
+const Postdetail = () => {
+
+    const post = useLoaderData();
+
+
+    return (
+        <div>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        </div>
+    );
+};
+
+export default Postdetail;
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import Laptops from './Components/Laptops/Laptops.jsx'
 import Users from './Components/Users/Users.jsx'
 import Users2 from './Components/Users2/Users2.jsx'
 import UserDetails from './Components/UserDetails/UserDetails.jsx'
+import Posts from './Components/Posts/Posts.jsx'
+import Postdetail from './Components/PostDetail/Postdetail.jsx'
 
 
 const usersPromise = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
@@ -34,8 +36,18 @@ const router = createBrowserRouter([{
     },
     {
       path: "users/:userId",
-      loader: ({params}) => fetch(),
+      loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
       Component: UserDetails
+    },
+    {
+      path: 'posts',
+      loader: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+      Component: Posts
+    },
+    {
+      path: 'posts/:postId',
+      loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+      Component: Postdetail
     }
   ]
 },
